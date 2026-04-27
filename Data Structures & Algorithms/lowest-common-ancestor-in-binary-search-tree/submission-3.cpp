dff7d754d33f2a795baba5f32eac0eb403929e65
Brad Nguyen <thaibinhbr97@gmail.com>
2026-04-27 11:42:12 -0400

    Add: lowest-common-ancestor-in-binary-search-tree - submission-3

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-3.cpp
@@ -0,0 +1,54 @@
+#include <cmath>
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    // int height(TreeNode* node) {
+    //     if (!node) return 0;
+    //     int leftHeight = height(node->left);
+    //     int rightHeight = height(node->right);
+    //     return 1 + max(leftHeight, rightHeight);
+    // }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // The logic for when p > q or p < q are the same so we only need to keep one
+        // Time: O(logn)
+        // Space: O(1) without recursive stack
+        // bool isPSmaller = p->val < q->val ? true : false;
+        // if (isPSmaller) {
+        //     if ((p->val < root->val) && (q->val < root->val)) {
+        //         return lowestCommonAncestor(root->left, p, q);
+        //     } else if ((p->val > root->val) && (q->val > root->val)) {
+        //         return lowestCommonAncestor(root->right, p, q);
+        //     } else {
+        //         return root;
+        //     }
+        // // p is now larger than q
+        // } else {
+        //     if ((p->val < root->val) && (q->val < root->val)) {
+        //         return lowestCommonAncestor(root->left, p, q);
+        //     } else if ((p->val > root->val) && (q->val > root->val)) {
+        //         return lowestCommonAncestor(root->right, p, q);
+        //     } else {
+        //         return root;
+        //     }            
+        // }
+
+        if ((p->val < root->val) && (q->val < root->val)) {
+            return lowestCommonAncestor(root->left, p, q);
+        } else if ((p->val > root->val) && (q->val > root->val)) {
+            return lowestCommonAncestor(root->right, p, q);
+        } else {
+            return root;
+        }      
+    }
+};
