f78878c5afe17ea61ad08b30c0c6e1531a6db6bc
Brad Nguyen <thaibinhbr97@gmail.com>
2026-05-23 19:28:25 -0400

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        unordered_map<int, double> positionTime; // key: position, value: hours to target
+        for (int i = 0; i < n; i++) {
+            positionTime[position[i]] = (double) (target - position[i]) / speed[i];
+        }
+        sort(position.begin(), position.end(), greater<int>()); // sort in descending order by position
+        stack<double> st;
+        for (int i = 0; i < n; i++) {
+            if (!st.empty() && positionTime[position[i]] <= st.top()) {
+                continue;
+            }
+            st.push(positionTime[position[i]]);
+            
+        }
+        return st.size();
+    }
+};
