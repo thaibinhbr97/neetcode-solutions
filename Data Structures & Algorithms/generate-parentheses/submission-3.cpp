92556cceb07fd1b3ae55484542906fa77761baac
Brad Nguyen <thaibinhbr97@gmail.com>
2026-06-20 15:52:19 -0400

    Add: generate-parentheses - submission-3

---
+++ Data Structures & Algorithms/generate-parentheses/submission-3.cpp
@@ -0,0 +1,85 @@
+// class Solution {
+// public:
+//     void backtrack(int n, vector<string>& result, string& path, int openParens, int closeParens) {
+//         // base case:
+//         if (openParens == n && closeParens == n) {
+//             result.push_back(path);
+//             return;
+//         }
+//         if (openParens > n || closeParens > n) {
+//             return;
+//         }
+//         if (openParens < n) {
+//             path.push_back('(');
+//             backtrack(n, result, path, openParens + 1, closeParens);
+//             path.pop_back();
+//         }
+//         if (closeParens < openParens) {
+//             path.push_back(')');
+//             backtrack(n, result, path, openParens, closeParens + 1);
+//             path.pop_back();
+//         }
+//     }
+// // public:
+// //     void backtrack(int n, vector<string>& result, string path, int openParens, int closeParens) {
+// //         // base case:
+// //         if (openParens == n && closeParens == n) {
+// //             result.push_back(path);
+// //             return;
+// //         }
+// //         if (openParens > n || closeParens > n) {
+// //             return;
+// //         }
+// //         if (openParens < n) {
+// //             backtrack(n, result, path + "(", openParens + 1, closeParens);
+// //         }
+// //         if (closeParens < openParens) {
+// //             backtrack(n, result, path + ")", openParens, closeParens + 1);
+// //         }
+// //     }
+//     vector<string> generateParenthesis(int n) {
+//         // (), n = 1
+//         // ()(), (()), n = 2
+//         // ()()(), ((())), (()()), ()(()), (())() n = 3
+//         // (), ((
+//         // 1. l(open) < n   
+//         // 2. l(open) > l(close)
+
+//         // 1.base: len of open == len of close == n -> append current option to the result
+//         // 2. l(open) > n || l(close) > n: return -> not want to explore anymore
+//         // 3. l(open) > l(close) -> add more close parens -> satisfy cond 
+//         // 4. l(close) > l(open): ()) -> never reach requirement -> not satisfy cond -> BACKTRACK until l(open) == l(close)
+//         vector<string> result;
+//         string path = "";
+//         backtrack(n, result, path, 0, 0);
+//         return result;
+//     }
+// };
+
+
+class Solution {
+public:
+    bool isValid(string s, int n) {
+        int balance = 0;
+        for (char c: s) {
+            balance += (c == '(') ? 1 : -1;
+            if (balance < 0) return false;
+        }
+        return balance == 0;
+    }
+    void dfs(string s, int n, vector<string>& result) {
+        if (s.size() == 2*n) {
+            if (isValid(s, n)) {
+                result.push_back(s);
+            }
+            return;
+        }
+        dfs(s + '(', n, result);
+        dfs(s + ')', n, result);
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        dfs("", n, result);
+        return result;
+    }
+};
