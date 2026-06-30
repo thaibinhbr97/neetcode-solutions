cd6281c83a3ff495b8e9b2b32360f05950dc3548
Brad Nguyen <thaibinhbr97@gmail.com>
2026-06-30 11:05:22 -0400

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    int dRow[4] = {-1, 1, 0, 0};
+    int dCol[4] = {0, 0, -1, 1};
+public:
+    bool isValid(int r, int c, int rows, int cols) {
+        if (r < 0 || c < 0 || r >= rows || c >= cols) {
+            return false;
+        }
+        return true;
+    }
+    void bfs(int r, int c, int rows, int cols, vector<vector<bool>>& visited, vector<vector<char>>& grid) {
+        queue<pair<int, int>> q;
+        q.push({r, c});
+        visited[r][c] = true;
+        while (!q.empty()) {
+            pair<int, int> front = q.front();
+            q.pop();
+            int r = front.first;
+            int c = front.second;
+            for (int i = 0; i < 4; i++) {
+                int ur = r + dRow[i];
+                int uc = c + dCol[i];
+                if (isValid(ur, uc, rows, cols) && grid[ur][uc] == '1' && !visited[ur][uc]) {
+                    visited[ur][uc] = true;                    
+                    q.push({ur, uc});
+                }
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+        int islands = 0;
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == '1' && !visited[r][c]) {
+                    bfs(r, c, rows, cols, visited, grid);
+                    islands++;
+                }
+            }
+        }
+        return islands;
+    }
+};
